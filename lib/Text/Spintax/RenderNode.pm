a76bd8ce35d2fd2d3754f28d0197be81a4bf22bb
sites <sites@n5.tnaventures.com>
2013-07-05 12:24:55 -0700

    DE: initial commit

---
+++ lib/Text/Spintax/RenderNode.pm
@@ -0,0 +1,75 @@
+package Text::Spintax::RenderNode;
+
+use Text::Spintax::Mo;
+
+has 'parent' => (
+   is => 'rw',
+);
+
+has 'children' => (
+   is => 'rw',
+);
+
+has 'weight' => (
+   is => 'rw',
+);
+
+has 'text' => (
+   is => 'rw',
+);
+
+has 'type' => (
+   is => 'rw',
+);
+
+sub render {
+   my $self = shift;
+   if ($self->type eq "text") {
+      return $self->text;
+   }
+   elsif ($self->type eq "spin") {
+      my $total = 0;
+      foreach my $child (@{$self->children}) {
+         $total += $child->weight;
+      }
+      my $rand = rand $total;
+      foreach my $child (@{$self->children}) {
+         $rand -= $child->weight;
+         $rand <= 0 and return $child->render;
+      }
+   }
+   elsif ($self->type eq "sequence") {
+      return join "", map { $_->render } @{$self->children};
+   }
+   else {
+      die "invalid type";
+   }
+}
+
+sub equal_path_weight {
+   my $self = shift;
+   $self->weight($self->num_paths);
+   foreach my $child (@{$self->children}) {
+      $child->equal_path_weight;
+   }
+}
+
+sub num_paths {
+   my $self = shift;
+   if ($self->type eq "spin") {
+      my $num_paths = 0;
+      foreach my $child (@{$self->children}) {
+         $num_paths += $child->num_paths;
+      }
+      return $num_paths;
+   }
+   else {
+      my $num_paths = 1;
+      foreach my $child (@{$self->children}) {
+         $num_paths *= $child->num_paths;
+      }
+      return $num_paths;
+   }
+}
+
+1;
